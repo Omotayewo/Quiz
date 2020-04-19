d7205aac4a0b4011f105bd7cdd394e95498f8700
Omotayewo <60750250+Omotayewo@users.noreply.github.com>
2020-04-20 00:33:18 +0100

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,109 @@
+var questions = [
+    {
+        question: 'Where is Faroe Island Located?',
+        choices: ['England', 'Denmark', 'Jamaica', 'Norway'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Where is Victoria Falls Located?',
+        choices: ['Scotland', 'Canada', 'Zambia', 'USA'],
+        correctAnswer: 2
+    },
+    {
+        question: 'Where is Moraine Lakes Located?',
+        choices: ['Canada', 'France', 'England', 'Germany'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Where is Mt. Kilimanjaro Located?',
+        choices: ['Zimbabwe', 'Tanzania', 'Sudan', 'Algeria'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Where is Mt. Fuji Located?',
+        choices: ['China', 'Japan', 'South Korea', 'North Korea'],
+        correctAnswer: 1
+    }
+];
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
+window.addEventListener('DOMContentLoaded', function(e){
+    displayCurrentQuestion();
+
+    var quizMessage = document.querySelector('.quizMessage');
+
+        quizMessage.style.display = 'none';
+
+    document.querySelector('.nextButton').addEventListener('click', function(){
+        
+        if(!quizOver){
+            var radioBtnsChecked = document.querySelector('input[type=radio]:checked');
+
+            if (radioBtnsChecked === null){
+                quizMessage.innerText = 'Please select an answer';
+                quizMessage.style.display = 'block';
+            } else {
+                console.log(radioBtnsChecked.value);
+                quizMessage.style.display = 'none';
+                if (parseInt(radioBtnsChecked.value) === questions[currentQuestion].correctAnswer){
+                    correctAnswers++;
+                }
+
+                currentQuestion++;
+
+                if (currentQuestion < questions.length){
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+                    document.querySelector('.nextButton').innerText = 'Play Again?';
+                    quizOver = true;
+                 }
+                }   
+        } else {
+            quizOver = false;
+            document.querySelector('.nextButton').innerText = 'Next';
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore();
+        }
+    });
+});
+
+function displayCurrentQuestion(){
+    console.log('In display current Questions');
+
+    var question = questions[currentQuestion].question;
+    var questionClass = document.querySelector('.quizContainer > .question');
+    var choiceList = document.querySelector('.quizContainer > .choiceList');
+    var numChoices = questions[currentQuestion].choices.length;
+
+    questionClass.innerText = question;
+    choiceList.innerHTML = '';
+
+    var choice;
+    for (i = 0; i < numChoices; i++){
+        choice = questions[currentQuestion].choices[i];
+        var li = document.createElement('li');
+            li.innerHTML = '<li><input type="radio" value="' + i + '" name="dynradio"/>' + choice + '</li>'
+        choiceList.appendChild(li);
+
+    }
+}
+
+function resetQuiz(){
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore(){
+    document.querySelector('.quizContainer > .result').innerText = 'You scored: ' + correctAnswers + ' out of ' + questions.length;
+    document.querySelector('.quizContainer > .result').style.display = 'block';
+}
+
+function hideScore(){
+    document.querySelector('.result').style.display = 'none';
+}
